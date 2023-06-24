5bc6a42f9bcfa000ed66412037bb4f8c2375c3de
dy-cl <dylanrcounsell@gmail.com>
2023-06-24 15:47:24 +0100

    Added posting functionality.

--- bingbleApp/views.py
+++ bingbleApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')  # Replace 'home' with the URL name of your home page
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
+
+def home(request):
+    posts = Post.objects.all()  # Fetch all posts from the database
+    return render(request, 'home.html', {'posts': posts})
